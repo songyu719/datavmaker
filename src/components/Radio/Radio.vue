d1607f1ee347141d5e8ad87c9555405e8e4a1244
lvzhiping <lv_zhiping@bq-health.com>
2021-02-25 17:17:26 +0800

    单选框赋值

--- src/components/Radio/Radio.vue
+++ src/components/Radio/Radio.vue
@@ -1,7 +1,7 @@
 <template>
   <el-form-item :prop="data.id" :label="data.options.name" :rules="data.rules">
-    <el-radio-group>
-      <el-radio v-for="item of data.options.arr" :key="item.value">{{ item.label }}</el-radio>
+    <el-radio-group v-model="fromdata[data.id]">
+      <el-radio v-for="item of data.options.arr" :key="item.value" :label="item.value">{{ item.label }}</el-radio>
     </el-radio-group>
   </el-form-item>
 </template>
